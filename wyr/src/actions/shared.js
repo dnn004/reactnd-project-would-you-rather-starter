b65b1e5a49109042e10c9efb826ba3b2244a5b80
dnn004 <dnn004@ucsd.edu>
2021-03-28 14:01:54 -0700

    Add initial changes

---
+++ wyr/src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import { receiveQuestions } from '../actions/questions'
+import { receiveUsers } from '../actions/users'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+}
